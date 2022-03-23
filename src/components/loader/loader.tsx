7ca7a146163df6e67212ff2ce688c3898a04b1e9
Masanin <gmasmin@gmail.com>
2022-03-23 15:28:42 +0300

    add_loader_component

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,15 @@
+import { Center, Spinner } from '@chakra-ui/react'
+import usePrimaryColor from 'hooks/usePrimaryColor'
+import { VFC } from 'react'
+
+export interface LoaderProps {}
+
+export const Loader: VFC<LoaderProps> = ({}) => {
+  const color = usePrimaryColor()
+
+  return (
+    <Center>
+      <Spinner thickness="6px" speed="0.75s" emptyColor="gray.200" color={color} size="xl" />
+    </Center>
+  )
+}
