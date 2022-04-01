60b51b33524d8f13dc7564189268a2e7019a9ae4
masanin <gmasmin@gmail.com>
2022-04-01 19:16:24 +0300

    add_hightlight_nav_item_logic

--- src/components/layout/nav-item/nav-item.tsx
+++ src/components/layout/nav-item/nav-item.tsx
@@ -1,14 +1,17 @@
-import { Flex, FlexProps, Icon, useColorModeValue } from '@chakra-ui/react'
-import { useHoverColor } from 'hooks/useHoverColor'
 import { FC } from 'react'
+import { Flex, FlexProps, Icon, useColorModeValue } from '@chakra-ui/react'
+import { Link, useMatch } from 'react-router-dom'
 import { IconType } from 'react-icons'
-import { Link } from 'react-router-dom'
-import { AllPaths } from 'routes'
+import { useHoverColor } from 'hooks/useHoverColor'
+import { AdminPaths, AllPaths, TeacherPaths } from 'routes'
+import { useTypedSelector } from 'redux-store/hooks'
+import { selectUserRoles } from 'redux-store/reducers/user.slice'
+import { isAdmin } from 'types/user.types'
 
 export interface NavItemProps extends FlexProps {
   path: AllPaths
   text?: string
-  icon: IconType
+  icon?: IconType
 }
 
 export const NavItem: FC<NavItemProps> = ({ icon, path, text, children, ...rest }) => {
@@ -16,29 +19,28 @@ export const NavItem: FC<NavItemProps> = ({ icon, path, text, children, ...rest 
 
   const iconHoverColor = useColorModeValue('gray.900', 'white')
 
+  const userRoles = useTypedSelector(selectUserRoles)
+
+  const parentPath = isAdmin(userRoles) ? AdminPaths.admin : TeacherPaths.teacher
+
+  const match = useMatch(`${parentPath}/${path}`)
+
   return (
-    // <Link to={path}>
     <Link to={path}>
       <Flex
         align="center"
         p="4"
         mx="4"
+        my="2"
         borderRadius="lg"
         role="group"
         cursor="pointer"
-        fontSize="20"
+        fontSize="18"
+        {...(match && hoverEffect)}
         _hover={hoverEffect}
         {...rest}
       >
-        {icon && (
-          <Icon
-            mr="4"
-            _groupHover={{
-              color: iconHoverColor,
-            }}
-            as={icon}
-          />
-        )}
+        {icon && <Icon mr="4" color={iconHoverColor} as={icon} />}
         {text || children}
       </Flex>
     </Link>
