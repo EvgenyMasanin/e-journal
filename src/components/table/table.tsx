e887011ef5158d63330c904d8a1b7439b4f49960
masanin <gmasmin@gmail.com>
2022-04-03 19:59:31 +0300

    add_isUpdating_state_to_table

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,8 +1,9 @@
 import { ReactNode } from 'react'
-import { Box, Table as TableUI } from '@chakra-ui/react'
+import { Flex, Box, Table as TableUI } from '@chakra-ui/react'
 import { TableBody } from 'components/table/table-body'
 import { TableHead } from 'components/table/table-head'
 import { FormWrapper } from './table-form-wrapper'
+import { Loader } from 'components/loader'
 import { SubmitErrorHandler, SubmitHandler, UseFormReturn } from 'react-hook-form'
 import { TableContextProvider } from './table-context-provider'
 
@@ -16,6 +17,7 @@ export interface TableProps<T extends ObjectWithId, S extends string[], K> {
   printCounter?: boolean
   printIds?: boolean
   isLoading?: boolean
+  isUpdating?: boolean
   editable?: boolean
   form?: UseFormReturn<K>
   onValid?: SubmitHandler<K>
@@ -25,6 +27,7 @@ export interface TableProps<T extends ObjectWithId, S extends string[], K> {
 }
 
 export const Table = <T extends ObjectWithId, S extends string[], K>({
+  isUpdating,
   editable,
   form,
   onValid,
@@ -32,7 +35,24 @@ export const Table = <T extends ObjectWithId, S extends string[], K>({
   ...props
 }: TableProps<T, S, K>) => {
   return (
-    <Box overflowX="auto">
+    <Box overflowX="auto" position="relative">
+      {isUpdating && (
+        <Flex
+          backdropFilter="blur(10px) "
+          h="full"
+          w="full"
+          position="absolute"
+          zIndex="1"
+          justify="center"
+          alignItems="top"
+          pt="15%"
+        >
+          <Box w="100" h="100">
+            <Loader />
+          </Box>
+        </Flex>
+      )}
+
       <FormWrapper editable={editable} useFormReturn={form} onValid={onValid} onInValid={onInValid}>
         <TableUI variant="simple">
           <TableContextProvider {...props} editable={editable}>
